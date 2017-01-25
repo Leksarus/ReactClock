3c50e5153ef5bb1f806daa40dd652c1cddcc4685
Leksarus <kamil.nawrocki5@gmail.com>
2017-01-25 22:36:43 +0100

    Initial commit

---
+++ app/components/Nav.jsx
@@ -0,0 +1,30 @@
+var React = require ('react');
+var {Link, IndexLink} = require('react-router');
+
+var Nav = () => {
+	return (
+		<div className="top-bar">
+			<div className="top-bar-left">
+				<ul className="menu">
+					<li className="menu-text">React Clock Application</li>
+					<li>
+						<IndexLink to="/" activeClassName="active">Timer</IndexLink>
+					</li>
+					<li>
+						<Link to="/" activeClassName="active">Countdown</Link>
+					</li>
+				</ul>
+			</div>
+			<div className="top-bar-right">
+				<ul className="menu">
+					<li className="menu-text">Made by</li>
+					<li className="menu-text">
+						<a href="https://github.com/Leksarus" target="_blank">Kamil Nawrocki</a>	
+					</li>
+				</ul>
+			</div>
+		</div>
+	);
+}
+
+module.exports = Nav;
